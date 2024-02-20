2bc72451ae994a9bb535e58284b8091bec3b06f6
anna-hallinder <anna.hallinder@iths.se>
2024-02-20 09:49:02 +0100

    2

---
+++ DataAccess/DataAccess/Entities/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace DataAccess.Entities;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Firstname { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Adress { get; set; }
+
+    public List<Product> Products { get; set; }
+}
---
+++ DataAccess/DataAccess/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace DataAccess.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Productname { get; set; }
+    public string Productdescription { get; set; }
+    public decimal Price { get; set; }
+    public string ProductCategory { get; set;}
+
+    public List<Customer> Customers { get; set; }
+}
