bce70c0422da707ad2e171ff178886788f28120a
Uberoy <143390218+Uberoy@users.noreply.github.com>
2024-02-20 10:24:56 +0100

    update

--- DataAccess/DataAccess/Entities/Customer.cs
+++ DataAccess/DataAccess/Entities/Customer.cs
@@ -3,7 +3,7 @@
 public class Customer
 {
     public int Id { get; set; }
-    public string Firstname { get; set; }
+    public string FirstName { get; set; }
     public string Surname { get; set; }
     public string Email { get; set; }
     public string Phone { get; set; }
--- DataAccess/DataAccess/Entities/Product.cs
+++ DataAccess/DataAccess/Entities/Product.cs
@@ -3,8 +3,8 @@
 public class Product
 {
     public int Id { get; set; }
-    public string Productname { get; set; }
-    public string Productdescription { get; set; }
+    public string ProductName { get; set; }
+    public string ProductDescription { get; set; }
     public decimal Price { get; set; }
     public string ProductCategory { get; set;}
 
