238ce0c58a1a98d5df6fced17d867322cf7c6fdf
Lotta Harmonen <lotta.harmonen@gmail.com>
2024-02-20 14:16:34 +0100

    CRUD complete

--- DataAccess/WebbStoreAPI/Program.cs
+++ DataAccess/WebbStoreAPI/Program.cs
@@ -1,4 +1,5 @@
 using DataAccess;
+using DataAccess.Entities;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -14,11 +15,101 @@ builder.Services.AddDbContext<StoreContext>(
 );
 
 
-// Add services to the container.
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.MapGet("/customers", (StoreContext context)
+    =>
+{
+    return context.Customer;
+});
 
+app.MapGet("/products", (StoreContext context)
+    =>
+{
+    return context.Product;
+});
+
+app.MapPost("/customers", (StoreContext context, Customer customer)
+    =>
+{
+    context.Add(customer);
+    context.SaveChanges();
+});
+
+app.MapPost("/products", (StoreContext context, Product product)
+    =>
+{
+    context.Add(product);
+    context.SaveChanges();
+});
+
+app.MapPut("/products/{id}", (StoreContext context, Product product, int id)
+    =>
+{
+    var productToUpdate = context.Product.FirstOrDefault(p => p.Id == id); 
+
+    productToUpdate.ProductName = product.ProductName;
+    productToUpdate.Price = product.Price;
+    productToUpdate.ProductCategory = product.ProductCategory;
+    productToUpdate.ProductDescription = product.ProductDescription;
+
+    context.SaveChanges();
+});
+
+app.MapPut("/customers/{id}", (StoreContext context, Customer customer, int id)
+    =>
+{
+    var customerToUpdate = context.Customer.FirstOrDefault(c => c.Id == id);
+
+    customerToUpdate.FirstName = customer.FirstName;
+    customerToUpdate.Surname = customer.Surname;
+    customerToUpdate.Adress = customer.Adress;
+    customerToUpdate.Email = customer.Email;
+    customerToUpdate.Phone = customer.Phone;
+
+    context.SaveChanges();
+});
+
+app.MapPatch("/customers/{id}", (StoreContext context, int id, string property, string value)
+    =>
+{
+    var customerToUpdate = context.Customer.FirstOrDefault(c => c.Id == id);
+
+    var propertyInfo = customerToUpdate.GetType().GetProperty(property);
+    propertyInfo.SetValue(customerToUpdate,Convert.ChangeType(value, propertyInfo.PropertyType),null);
+
+
+    context.SaveChanges();
+});
+
+app.MapPatch("/products/{id}", (StoreContext context, int id, string property, string value)
+    =>
+{
+    var productToUpdate = context.Product.FirstOrDefault(c => c.Id == id);
+
+    var propertyInfo = productToUpdate.GetType().GetProperty(property);
+    propertyInfo.SetValue(productToUpdate, Convert.ChangeType(value, propertyInfo.PropertyType), null);
+
+    context.SaveChanges();
+});
+
+
+app.MapDelete("/customers/{id}", (StoreContext context, int id)
+    =>
+{
+    var customerToDelete = context.Customer.FirstOrDefault(c => c.Id == id);
+    context.Customer.Remove(customerToDelete);
+
+    context.SaveChanges();
+});
+
+app.MapDelete("/products/{id}", (StoreContext context, int id)
+    =>
+{
+    var productToDelete = context.Product.FirstOrDefault(p => p.Id == id);
+    context.Product.Remove(productToDelete);
+
+    context.SaveChanges();
+});
 
 app.Run();
